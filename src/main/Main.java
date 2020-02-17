564bafd516d2c21ec3d86df25fdbc5d6c7005416
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-02-17 23:04:05 +0530

    fields created in Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("welcome to Adam's contact list");
+        System.out.println("Press 1 to add a new contact\n" +
+                "Press 2 to view all contacts\n" +
+                "Press 3 to search for a contact\n" +
+                "Press 4 to delete a contact\n" +
+                "Press 5 to exit program ");
+        int number = scanner.nextInt();
+
     }
 }
