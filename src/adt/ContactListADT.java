2ecd9d2aaf314b09587e26f2a2db237d9f3a1988
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-02-18 02:05:23 +0530

    perameter&datatype of delete method changed to String in ContactListADT

--- src/adt/ContactListADT.java
+++ src/adt/ContactListADT.java
@@ -3,7 +3,7 @@ package adt;
 public interface ContactListADT<Person> {
     boolean add(Person person);
 
-    boolean delete(Person person);
+    boolean delete(String personFirstName);
 
     boolean search(String name);
 
