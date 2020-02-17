cb6f4c1507d73c866e98e3a74a0f9f65188fa9ed
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-02-17 23:58:23 +0530

    Node class created

--- src/definition/ContactList.java
+++ src/definition/ContactList.java
@@ -3,6 +3,32 @@ package definition;
 import adt.ContactListADT;
 
 public class ContactList<Person> implements ContactListADT<Person> {
+    private static class Node<Person> {
+        private Person data;
+        private Node<Person> next;
+
+        public Node(Person data, Node<Person> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Person getData() {
+            return data;
+        }
+
+        public void setData(Person data) {
+            this.data = data;
+        }
+
+        public Node<Person> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<Person> next) {
+            this.next = next;
+        }
+    }
+
     @Override
     public boolean add(Person person) {
         return false;
